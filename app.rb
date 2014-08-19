f0f9c46351b163ffc6f3d1e06a8e71a7c9e2b777
Jason Barnett <jason.w.barnett@gmail.com>
2014-08-19 14:29:09 -0700

    removed unnecessary line

--- app.rb
+++ app.rb
@@ -4,6 +4,5 @@ require 'sinatra/json'
 set :protection, :except => [:json_csrf]
 
 get '*' do
-  #erb :index, locals: {env: env}
   json({"ip" => env['REMOTE_ADDR'], "agent" => env['HTTP_USER_AGENT']})
 end
