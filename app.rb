e9960393587b5cf2b6bd450107c0ff8cd0a72306
Jason Barnett <jason.w.barnett@gmail.com>
2014-08-19 16:03:14 -0700

    added robots.txt route

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra/json'
 
 set :protection, :except => [:json_csrf]
 
+get '/robots.txt' do
+end
+
 get '*' do
   json({"ip" => env['REMOTE_ADDR'], "agent" => env['HTTP_USER_AGENT']})
 end
