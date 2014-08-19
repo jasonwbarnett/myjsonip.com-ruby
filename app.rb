dfc057ef11c9ada4be21cfd3b15aa3143dffb9aa
Jason Barnett <jason.w.barnett@gmail.com>
2014-08-19 16:18:04 -0700

    added yaml support

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'yaml'
 
 set :protection, :except => [:json_csrf]
 
 get %r{/(robots\.txt|favicon\.ico)} do
 end
 
+body = {"ip" => env['REMOTE_ADDR'], "agent" => env['HTTP_USER_AGENT']}
+
+get %r{/ya?ml} do
+  body.to_yaml
+end
+
+## Respond with JSON by default
 get '*' do
-  json({"ip" => env['REMOTE_ADDR'], "agent" => env['HTTP_USER_AGENT']})
+  json(body)
 end
