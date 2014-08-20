e4e4fe7ee0acd479c724e9d8efb67af3560668c6
Jason Barnett <jason.w.barnett@gmail.com>
2014-08-20 09:20:49 -0700

    added some specific routes for information. /all, /agent, /

--- app.rb
+++ app.rb
@@ -4,21 +4,60 @@ require 'yaml'
 
 set :protection, :except => [:json_csrf]
 
+helpers do
+  def gen_ip(env)
+    body = {"ip" => env['REMOTE_ADDR']}
+  end
+
+  def gen_agent(env)
+    body = {"agent" => env['HTTP_USER_AGENT']}
+  end
+
+  def gen_all(env)
+    ip    = gen_ip(env)
+    agent = gen_agent(env)
+
+    properties = [ip, agent]
+
+    body = properties.inject({}) {|memo,x| memo.merge!(x); memo}
+  end
+end
+
 get %r{/(robots\.txt|favicon\.ico)} do
 end
 
-def gen_body(env)
-  body = {"ip" => env['REMOTE_ADDR'], "agent" => env['HTTP_USER_AGENT']}
+get %r{/all/ya?ml}i do
+  content_type :yaml
+  body = gen_all(env)
+  body.to_yaml
 end
 
-get %r{/ya?ml} do
+get '/all*' do
+  body = gen_all(env)
+  json(body)
+end
+
+get %r{/agent/ya?ml}i do
   content_type :yaml
-  body = gen_body(env)
+  body = gen_agent(env)
+  body.to_yaml
+end
+
+get '/agent*' do
+  body = gen_agent(env)
+  json(body)
+end
+
+## Respond with ip via YAML be default
+get %r{/ya?ml.*}i do
+  content_type :yaml
+  body = gen_ip(env)
   body.to_yaml
 end
 
 ## Respond with JSON by default
 get '*' do
-  body = gen_body(env)
+  body = gen_ip(env)
   json(body)
 end
+
